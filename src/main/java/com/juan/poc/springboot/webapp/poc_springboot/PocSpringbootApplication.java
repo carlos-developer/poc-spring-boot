6ce0c5499836c75717ccab303e8c7fa70ccea89b
Juan Carlos <juansuarezmarin@gmail.com>
2024-11-23 07:58:08 -0500

    initial commit

---
+++ src/main/java/com/juan/poc/springboot/webapp/poc_springboot/PocSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.juan.poc.springboot.webapp.poc_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juan/poc/springboot/webapp/poc_springboot/PocSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juan.poc.springboot.webapp.poc_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

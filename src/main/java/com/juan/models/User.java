96094bf98d90f7e53afb1053a92538dfddd93fb7
Juan Carlos <juansuarezmarin@gmail.com>
2024-12-03 21:18:10 -0500

    #24 Directiva each (foreach) de thymeleaf

--- src/main/java/com/juan/models/User.java
+++ src/main/java/com/juan/models/User.java
@@ -5,11 +5,19 @@ public class User {
     private String name;
     private String lastName;
     private String email;
+    public  User(){}
     
     public User(String name, String lastName) {
         this.name = name;
         this.lastName = lastName;
     }
+
+    public User(String name, String lastName, String email) {
+        this.name = name;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
     public String getName() {
         return name;
     }
--- src/main/java/com/juan/poc/springboot/webapp/poc_springboot/controllers/UserController.java
+++ src/main/java/com/juan/poc/springboot/webapp/poc_springboot/controllers/UserController.java
@@ -1,20 +1,14 @@
 package com.juan.poc.springboot.webapp.poc_springboot.controllers;
 
 import com.juan.models.User;
-import com.juan.models.dto.UserDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.ArrayList;
 import java.util.List;
-
 
 @Controller
 public class UserController {
-    
-
 
     @GetMapping("/details")
     public String detailsMap(Model model) {
@@ -26,12 +20,10 @@ public class UserController {
 
     @GetMapping("/list")
     public String list(ModelMap model) {
-        List<User> users = new ArrayList<>();
+        List<User> users = List.of(new User("pepa", "Gonzalez", "pep@gmail.com"), new User("lalo", "perez"), new User("Juanita", "Roe"));
 
         model.addAttribute("users", users);
         model.addAttribute("title", "Listado de usuarios");
         return "list";
     }
-
-    
 }

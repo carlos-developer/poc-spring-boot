2c38748769bf65a057b6f22e08ec95c4ad62c4e0
Juan Carlos <juansuarezmarin@gmail.com>
2025-02-25 14:35:58 -0500

    #69 Registrando componentes items de la factura

--- src/main/java/com/juan/poc/springboot/webapp/poc_springboot/AppConfig.java
+++ src/main/java/com/juan/poc/springboot/webapp/poc_springboot/AppConfig.java
@@ -1,9 +1,22 @@
 package com.juan.poc.springboot.webapp.poc_springboot;
 
+import com.juan.poc.springboot.webapp.poc_springboot.models.Item;
+import com.juan.poc.springboot.webapp.poc_springboot.models.Product;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @PropertySource("classpath:data.properties")
 public class AppConfig {
+
+    @Bean
+    List<Item> itemsInvoice() {
+        Product p1 = new Product("Camara Sony", 800);
+        Product p2 = new Product("Bicicleta Bianchi 26", 1200);
+        return Arrays.asList(new Item(p1, 2), new Item(p2, 4));
+    }
 }
